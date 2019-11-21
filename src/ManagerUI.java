316f50783c34f343f1d15c2f88e09f8a8c76d3d1
det0009 <54649211+det0009@users.noreply.github.com>
2019-11-20 23:56:36 -0600

    Add files via upload
    Project2 src files updated

--- src/ManagerUI.java
+++ src/ManagerUI.java
@@ -1,22 +1,21 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainUI {
+public class ManagerUI {
     public JFrame view;
 
-    public JButton btnManageProduct = new JButton("Manage Product database");
-    public JButton btnManageCustomer = new JButton("Manage Customer database");
-    public JButton btnAddPurchase = new JButton("Add New Purchase");
-   // public JButton btnUpdateProduct = new JButton("Update Product Information");
+    public JButton btnManageCustomer = new JButton("Manage Customers");
+    public JButton btnManageProduct = new JButton("Manage Products");
+    public JButton btnManagePurchase = new JButton("Manage Purchase");
 
-
-    public MainUI() {
+    public ManagerUI() {
         this.view = new JFrame();
 
         view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        view.setTitle("Store Management System");
+        view.setTitle("Store Management System - Manager View");
         view.setSize(1000, 600);
         view.getContentPane().setLayout(new BoxLayout(view.getContentPane(), BoxLayout.PAGE_AXIS));
 
@@ -28,38 +27,35 @@ public class MainUI {
         JPanel panelButtons = new JPanel(new FlowLayout());
         panelButtons.add(btnManageProduct);
         panelButtons.add(btnManageCustomer);
-        panelButtons.add(btnAddPurchase);
+        panelButtons.add(btnManagePurchase);
 
         view.getContentPane().add(panelButtons);
 
-        btnManageProduct.addActionListener(new ActionListener() {
 
+        btnManageProduct.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                ManageProductUI ap = new ManageProductUI();
-                ap.run();
+                ManageProductUI ui = new ManageProductUI();
+                ui.run();
             }
         });
 
         btnManageCustomer.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                ManageCustomerUI ac = new ManageCustomerUI();
-                ac.run();
+            public void actionPerformed(ActionEvent actionEvent) {
+                ManageCustomerUI ui = new ManageCustomerUI();
+                ui.run();
             }
         });
 
 
-        btnAddPurchase.addActionListener(new ActionListener() {
-
+        btnManagePurchase.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                AddPurchaseUI apur = new AddPurchaseUI();
-                apur.run();
+                ManagePurchaseUI ui = new ManagePurchaseUI();
+                ui.run();
             }
         });
-
-
 
     }
 }
